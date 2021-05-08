aba678c9f26f9b87b03d456a176f194d9b532249
kongxiangjin <1351882069@qq.com>
2021-05-08 18:00:19 +0800

    mybatis

--- mybatis-demo-01/src/test/java/MaBatisTest.java
+++ mybatis-demo-01/src/test/java/MaBatisTest.java
@@ -1,4 +1,5 @@
 import com.kxj.entity.Blog;
+import org.apache.commons.lang3.RandomStringUtils;
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
@@ -7,6 +8,8 @@ import org.junit.Test;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author xiangjin.kong
@@ -16,11 +19,72 @@ import java.io.InputStream;
 public class MaBatisTest {
 
     @Test
-    public void test() throws IOException {
+    public void testSelect() throws IOException {
         InputStream inputStream = Resources.getResourceAsStream("sqlMapConfig.xml");
         SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
         SqlSession session = sqlSessionFactory.openSession();
         Blog blog = session.selectOne("blog.selectBlog", 1);
         System.out.println(blog);
+    }
+
+    @Test
+    public void testSelectAll() throws IOException {
+        InputStream inputStream = Resources.getResourceAsStream("sqlMapConfig.xml");
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        SqlSession session = sqlSessionFactory.openSession();
+        List<Blog> blog = session.selectList("blog.selectList");
+        System.out.println(blog);
+    }
+
+    @Test
+    public void testInsert() throws IOException {
+        InputStream inputStream = Resources.getResourceAsStream("sqlMapConfig.xml");
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        SqlSession session = sqlSessionFactory.openSession();
+        Blog blog = new Blog();
+        blog.setName("lisi");
+        int effectCol = session.insert("blog.insertBlog", blog);
+        session.commit();
+        System.out.println(blog.getId());
+    }
+
+    @Test
+    public void testDelete() throws IOException {
+        InputStream inputStream = Resources.getResourceAsStream("sqlMapConfig.xml");
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        SqlSession session = sqlSessionFactory.openSession();
+        int effectCol = session.insert("blog.deleteBlog", 2);
+        System.out.println(effectCol);
+        session.commit();
+    }
+
+    @Test
+    public void testUpdate() throws IOException {
+        InputStream inputStream = Resources.getResourceAsStream("sqlMapConfig.xml");
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        SqlSession session = sqlSessionFactory.openSession();
+        int effectCol = session.insert("blog.updateBlog", "test");
+        System.out.println(effectCol);
+        session.commit();
+    }
+
+
+    @Test
+    public void batchInsert() throws IOException {
+        InputStream inputStream = Resources.getResourceAsStream("sqlMapConfig.xml");
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        SqlSession session = sqlSessionFactory.openSession();
+        List<Blog> list = new ArrayList<Blog>();
+        for (int j = 0; j < 50; j++) {
+            for (int i = 0; i < 10000; i++) {
+                Blog blog = new Blog();
+                String filename = RandomStringUtils.randomAlphanumeric(10);
+                blog.setName(filename);
+                list.add(blog);
+            }
+            int effectCol = session.insert("blog.batchInsert", list);
+            session.commit();
+        }
+
     }
 }
