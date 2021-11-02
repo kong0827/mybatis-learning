4254dc75cff707e20432ee18abab2b39691a1d3d
kongxiangjin <1351882069@qq.com>
2021-11-03 00:41:41 +0800

    feat: mybatis-generator生成器

---
+++ mybatis-auto-generator/src/main/java/AutoGeneratorApplication.java
@@ -0,0 +1,17 @@
+import com.baomidou.mybatisplus.generator.FastAutoGenerator;
+import com.baomidou.mybatisplus.generator.config.OutputFile;
+import com.baomidou.mybatisplus.generator.engine.FreemarkerTemplateEngine;
+
+import java.util.Collections;
+
+/**
+ * @author kxj
+ * @date 2021/11/3 00:30
+ * @desc
+ */
+public class AutoGeneratorApplication {
+
+    public static void main(String[] args) {
+        
+    }
+}
